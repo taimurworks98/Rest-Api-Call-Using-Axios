925a54d6ed2caf6c482a4bc6ccb892dae8d4a777
Taimur Malik <you@domain.com>
2021-08-20 12:43:03 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import List from './components/List';
+import withListLoading from './components/withListLoading';
+
+function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `https://api.github.com/users/taimurworks98/repos`;
+    axios.get(apiUrl).then((repos) => {
+      const allRepos = repos.data;
+      setAppState({ loading: false, repos: allRepos });
+    });
+  }, [setAppState]);
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>My Repositories</h1>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <footer>
+        <div className='footer'>
+          Built{' '}with{' '}
+          <span role='img' aria-label='love'>
+            💚
+          </span>{' '}
+          by Taimoor Malik
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
